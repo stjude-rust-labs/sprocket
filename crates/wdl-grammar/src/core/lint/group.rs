05a8b4de70e3eaa69fdd99d92c44cd509233e193
Clay McLeod <clay.l.mcleod@gmail.com>
2023-11-22 22:13:56 -0600

    revise: adds the `Pedantic` lint group

--- crates/wdl-grammar/src/core/lint/group.rs
+++ crates/wdl-grammar/src/core/lint/group.rs
@@ -5,12 +5,16 @@
 pub enum Group {
     /// Rules associated with the style of an input.
     Style,
+
+    /// Rules often considered overly opinionated.
+    Pedantic,
 }
 
 impl std::fmt::Display for Group {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Group::Style => write!(f, "Style"),
+            Group::Pedantic => write!(f, "Pedantic"),
         }
     }
 }
