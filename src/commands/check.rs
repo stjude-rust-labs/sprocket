b3dbcee9e3c90e25a6a2262153ab405be2568512
Andrew Frantz <andrew.frantz@stjude.org>
2024-12-17 17:50:57 -0500

    chore: update sprocket to latest WDL crate
    This also adds the ability to check/lint URLs

--- src/commands/check.rs
+++ src/commands/check.rs
@@ -1,7 +1,6 @@
 //! Implementation of the check and lint commands.
 
-use std::borrow::Cow;
-use std::path::PathBuf;
+use std::fs;
 use std::sync::Arc;
 use std::time::Duration;
 
@@ -12,7 +11,9 @@ use codespan_reporting::files::SimpleFile;
 use codespan_reporting::term::emit;
 use indicatif::ProgressBar;
 use indicatif::ProgressStyle;
+use url::Url;
 use wdl::analysis::Analyzer;
+use wdl::analysis::path_to_uri;
 use wdl::analysis::rules;
 use wdl::ast::Diagnostic;
 use wdl::ast::Severity;
@@ -31,10 +32,12 @@ const PROGRESS_BAR_DELAY: Duration = Duration::from_secs(2);
 pub struct Common {
     /// The files or directories to check.
     #[arg(required = true)]
-    pub paths: Vec<PathBuf>,
+    #[clap(value_name = "PATHs and/or URLs")]
+    pub files: Vec<String>,
 
-    /// A single rule ID to except from running. Can be specified multiple
-    /// times.
+    /// A single rule ID to except from running.
+    ///
+    /// Can be specified multiple times.
     #[arg(short, long, value_name = "RULE")]
     pub except: Vec<String>,
 
@@ -122,7 +125,20 @@ pub async fn check(args: CheckArgs) -> anyhow::Result<()> {
         },
     );
 
-    analyzer.add_documents(args.common.paths).await?;
+    for file in &args.common.files {
+        if let Ok(url) = Url::parse(file) {
+            analyzer.add_document(url).await?;
+        } else if fs::metadata(file)
+            .with_context(|| format!("failed to read metadata for file `{file}`"))?
+            .is_dir()
+        {
+            analyzer.add_directory(file.into()).await?;
+        } else if let Some(url) = path_to_uri(file) {
+            analyzer.add_document(url).await?;
+        } else {
+            bail!("failed to convert `{file}` to a URI", file = file)
+        }
+    }
 
     let bar = ProgressBar::new(0);
     bar.set_style(
@@ -143,30 +159,41 @@ pub async fn check(args: CheckArgs) -> anyhow::Result<()> {
     let mut warning_count = 0;
     let mut note_count = 0;
     for result in &results {
-        let path = result.uri().to_file_path().ok();
-
         // Attempt to strip the CWD from the result path
-        let path = match (&cwd, &path) {
-            // Only display diagnostics for local files.
-            (_, None) => continue,
-            // Use just the path if there's no CWD
-            (None, Some(path)) => path.to_string_lossy(),
-            // Strip the CWD from the path
-            (Some(cwd), Some(path)) => path.strip_prefix(cwd).unwrap_or(path).to_string_lossy(),
+        let uri = result.document().uri();
+        let scheme = uri.scheme();
+        let uri = match (cwd.clone(), scheme) {
+            (Some(cwd), "file") => uri
+                .to_string()
+                .strip_prefix(cwd.to_str().unwrap())
+                .unwrap_or(
+                    uri.to_file_path()
+                        .expect("failed to convert file URI to file path")
+                        .to_string_lossy()
+                        .as_ref(),
+                )
+                .to_string(),
+            (_, "file") => uri
+                .to_file_path()
+                .expect("failed to convert file URI to file path")
+                .to_string_lossy()
+                .to_string(),
+            _ => uri.to_string(),
         };
 
-        let diagnostics: Cow<'_, [Diagnostic]> = match result.parse_result().error() {
-            Some(e) => vec![Diagnostic::error(format!("failed to read `{path}`: {e:#}"))].into(),
-            None => result.diagnostics().into(),
+        let diagnostics = match result.error() {
+            Some(e) => &[Diagnostic::error(format!("failed to read `{uri}`: {e:#}"))],
+            None => result.document().diagnostics(),
         };
 
         if !diagnostics.is_empty() {
-            let source = result
-                .parse_result()
-                .root()
-                .map(|n| SyntaxNode::new_root(n.clone()).text().to_string())
-                .unwrap_or(String::new());
-            let file = SimpleFile::new(path, source);
+            let file = SimpleFile::new(
+                uri,
+                SyntaxNode::new_root(result.document().node().syntax().green().into())
+                    .text()
+                    .to_string(),
+            );
+
             for diagnostic in diagnostics.iter() {
                 match diagnostic.severity() {
                     Severity::Error => error_count += 1,
@@ -182,17 +209,17 @@ pub async fn check(args: CheckArgs) -> anyhow::Result<()> {
 
     if error_count > 0 {
         bail!(
-            "aborting due to previous {error_count} error{s}",
+            "failing due to {error_count} error{s}",
             s = if error_count == 1 { "" } else { "s" }
         );
     } else if args.common.deny_warnings && warning_count > 0 {
         bail!(
-            "aborting due to previous {warning_count} warning{s} (`--deny-warnings` was specified)",
+            "failing due to {warning_count} warning{s} (`--deny-warnings` was specified)",
             s = if warning_count == 1 { "" } else { "s" }
         );
     } else if args.common.deny_notes && note_count > 0 {
         bail!(
-            "aborting due to previous {note_count} note{s} (`--deny-notes` was specified)",
+            "failing due to {note_count} note{s} (`--deny-notes` was specified)",
             s = if note_count == 1 { "" } else { "s" }
         );
     }
