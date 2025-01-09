17fdadd06881b27e218dcb13c9899f923da8fac8
Andrew Frantz <andrew.frantz@stjude.org>
2025-01-09 16:27:34 -0500

    Update validate.rs

--- src/commands/validate.rs
+++ src/commands/validate.rs
@@ -1,23 +1,12 @@
 //! Implementation of the `validate-inputs` command.
 
-use std::borrow::Cow;
-use std::path::Path;
 use std::path::PathBuf;
 
-use anyhow::Context;
-use anyhow::bail;
 use clap::Parser;
-use codespan_reporting::files::SimpleFile;
-use codespan_reporting::term::Config;
-use codespan_reporting::term::emit;
-use codespan_reporting::term::termcolor::Buffer;
-use url::Url;
-use wdl::analysis::path_to_uri;
-use wdl::ast::Diagnostic;
-use wdl::ast::Severity;
-use wdl::engine::Inputs;
+use wdl::cli::validate_inputs as wdl_validate_inputs;
 
-use crate::analyze;
+use crate::Mode;
+use crate::get_display_config;
 
 /// Arguments for the `validate-inputs` command.
 #[derive(Parser, Debug)]
@@ -31,6 +20,14 @@ pub struct ValidateInputsArgs {
     /// The path to the input JSON file.
     #[arg(short, long, value_name = "JSON")]
     pub inputs: PathBuf,
+
+    /// Disables color output.
+    #[arg(long)]
+    pub no_color: bool,
+
+    /// The report mode.
+    #[arg(short = 'm', long, default_value_t, value_name = "MODE")]
+    pub report_mode: Mode,
 }
 
 /// Validates the inputs for a task or workflow.
@@ -39,93 +36,9 @@ pub struct ValidateInputsArgs {
 /// * Every supplied input is correctly typed.
 /// * No extraneous inputs are provided.
 pub async fn validate_inputs(args: ValidateInputsArgs) -> anyhow::Result<()> {
-    let ValidateInputsArgs { document, inputs } = args;
+    let (config, mut stream) = get_display_config(args.report_mode, args.no_color);
 
-    if Path::new(&document).is_dir() {
-        bail!("expected a WDL document, found a directory");
-    }
-
-    let results = analyze(&document, vec![], false, false).await?;
-
-    let uri = if let Ok(uri) = Url::parse(&document) {
-        uri
-    } else {
-        path_to_uri(&document).expect("file should be a local path")
-    };
-
-    let result = results
-        .iter()
-        .find(|r| **r.document().uri() == uri)
-        .context("failed to find document in analysis results")?;
-    let analyzed_document = result.document();
-
-    let diagnostics: Cow<'_, [Diagnostic]> = match result.error() {
-        Some(e) => vec![Diagnostic::error(format!(
-            "failed to read `{document}`: {e:#}"
-        ))]
-        .into(),
-        None => analyzed_document.diagnostics().into(),
-    };
-
-    if let Some(diagnostic) = diagnostics.iter().find(|d| d.severity() == Severity::Error) {
-        let source = result.document().node().syntax().text().to_string();
-        let file = SimpleFile::new(&document, &source);
-
-        let mut buffer = Buffer::no_color();
-        emit(
-            &mut buffer,
-            &Config::default(),
-            &file,
-            &diagnostic.to_codespan(),
-        )
-        .expect("should emit");
-
-        let diagnostic: String = String::from_utf8(buffer.into_inner()).expect("should be UTF-8");
-        bail!("document `{document}` contains at least one diagnostic error:\n{diagnostic}");
-    }
-
-    let result = match Inputs::parse(analyzed_document, inputs) {
-        Ok(Some((name, inputs))) => match inputs {
-            Inputs::Task(inputs) => {
-                match inputs
-                    .validate(
-                        analyzed_document,
-                        analyzed_document
-                            .task_by_name(&name)
-                            .expect("task should exist"),
-                            None,
-                    )
-                    .with_context(|| {
-                        format!("failed to validate inputs for task `{name}`", name = name)
-                    }) {
-                    Ok(()) => String::new(),
-                    Err(e) => format!("{e:?}"),
-                }
-            }
-            Inputs::Workflow(inputs) => {
-                let workflow = analyzed_document.workflow().expect("workflow should exist");
-                match inputs
-                    .validate(analyzed_document, workflow, None)
-                    .with_context(|| {
-                        format!(
-                            "failed to validate inputs for workflow `{name}`",
-                            name = name
-                        )
-                    }) {
-                    Ok(()) => String::new(),
-                    Err(e) => format!("{e:?}"),
-                }
-            }
-        },
-        Ok(None) => String::new(),
-        Err(e) => format!("{e:?}"),
-    };
-
-    if !result.is_empty() {
-        bail!("failed to validate inputs:\n{result}");
-    } else {
-        println!("inputs are valid");
-    }
-
+    wdl_validate_inputs(&args.document, &args.inputs, &mut stream, &config).await?;
+    println!("All inputs are valid");
     anyhow::Ok(())
 }
