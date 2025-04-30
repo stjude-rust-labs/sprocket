3f77d1a694e0e202114b92a103ac6f29f8b80033
Andrew Thrasher <adthrasher@gmail.com>
2025-04-30 14:34:12 -0400

    chore: apply PR feedback

--- src/config.rs
+++ src/config.rs
@@ -6,7 +6,10 @@ use serde::Deserialize;
 use serde::Serialize;
 
 use crate::Mode;
-
+use crate::commands::Commands;
+use crate::commands::check;
+use crate::commands::format;
+use crate::commands::validate;
 /// Represents the configuration for the Sprocket CLI tool.
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case", deny_unknown_fields)]
@@ -36,12 +39,6 @@ pub struct CommonConfig {
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case", deny_unknown_fields)]
 pub struct FormatConfig {
-    /// Disables color output.
-    pub no_color: bool,
-    /// Enable or disable color output.
-    pub color: bool,
-    /// The report mode.
-    pub report_mode: Option<Mode>,
     /// Use tabs for indentation (default is spaces).
     pub with_tabs: bool,
     /// The number of spaces to use for indentation levels (default is 4).
@@ -93,99 +90,91 @@ impl Config {
     }
 
     /// Merge the current configuration with commandline arguments.
-    pub fn merge_args(&self, args: crate::commands::Commands) -> crate::commands::Commands {
+    pub fn merge_args(&self, args: Commands) -> Commands {
         // Merge the common configuration with the commandline arguments
         match args {
-            crate::commands::Commands::Format(format_args) => {
-                crate::commands::Commands::Format(crate::commands::format::FormatArgs {
-                    path: format_args.path,
-                    no_color: format_args.no_color || !self.common_config.color,
-                    report_mode: match format_args.report_mode {
+            Commands::Format(format_args) => Commands::Format(format::FormatArgs {
+                path: format_args.path,
+                no_color: format_args.no_color || !self.common_config.color,
+                report_mode: match format_args.report_mode {
+                    Some(mode) => Some(mode),
+                    None => self.common_config.report_mode,
+                },
+                with_tabs: format_args.with_tabs || self.format_config.with_tabs,
+                indentation_size: match format_args.indentation_size {
+                    Some(size) => Some(size),
+                    None => self.format_config.indentation_size,
+                },
+                max_line_length: format_args
+                    .max_line_length
+                    .or(self.format_config.max_line_length),
+                mode: format_args.mode,
+            }),
+            Commands::Check(check_args) => Commands::Check(check::CheckArgs {
+                common: check::Common {
+                    file: check_args.common.file,
+                    except: check_args
+                        .common
+                        .except
+                        .into_iter()
+                        .chain(self.check_config.except.clone())
+                        .collect(),
+                    deny_warnings: check_args.common.deny_warnings
+                        || self.check_config.deny_warnings,
+                    deny_notes: check_args.common.deny_notes || self.check_config.deny_notes,
+                    single_document: check_args.common.single_document
+                        || self.check_config.single_document,
+                    show_remote_diagnostics: check_args.common.show_remote_diagnostics
+                        || self.check_config.show_remote_diagnostics,
+                    shellcheck: check_args.common.shellcheck || self.check_config.shellcheck,
+                    hide_notes: check_args.common.hide_notes || self.check_config.hide_notes,
+                    no_color: check_args.common.no_color || !self.common_config.color,
+                    report_mode: match check_args.common.report_mode {
                         Some(mode) => Some(mode),
                         None => self.common_config.report_mode,
                     },
-                    with_tabs: format_args.with_tabs || self.format_config.with_tabs,
-                    indentation_size: match format_args.indentation_size {
-                        Some(size) => Some(size),
-                        None => self.format_config.indentation_size,
-                    },
-                    max_line_length: format_args
-                        .max_line_length
-                        .or(self.format_config.max_line_length),
-                    mode: format_args.mode,
-                })
-            }
-            crate::commands::Commands::Check(check_args) => {
-                crate::commands::Commands::Check(crate::commands::check::CheckArgs {
-                    common: crate::commands::check::Common {
-                        file: check_args.common.file,
-                        except: check_args
-                            .common
-                            .except
-                            .into_iter()
-                            .chain(self.check_config.except.clone())
-                            .collect(),
-                        deny_warnings: check_args.common.deny_warnings
-                            || self.check_config.deny_warnings,
-                        deny_notes: check_args.common.deny_notes || self.check_config.deny_notes,
-                        single_document: check_args.common.single_document
-                            || self.check_config.single_document,
-                        show_remote_diagnostics: check_args.common.show_remote_diagnostics
-                            || self.check_config.show_remote_diagnostics,
-                        shellcheck: check_args.common.shellcheck || self.check_config.shellcheck,
-                        hide_notes: check_args.common.hide_notes || self.check_config.hide_notes,
-                        no_color: check_args.common.no_color || !self.common_config.color,
-                        report_mode: match check_args.common.report_mode {
-                            Some(mode) => Some(mode),
-                            None => self.common_config.report_mode,
-                        },
-                    },
-                    lint: check_args.lint,
-                })
-            }
-            crate::commands::Commands::ValidateInputs(validate_args) => {
-                crate::commands::Commands::ValidateInputs(
-                    crate::commands::validate::ValidateInputsArgs {
-                        document: validate_args.document,
-                        inputs: validate_args.inputs,
-                        no_color: validate_args.no_color || !self.common_config.color,
-                        report_mode: match validate_args.report_mode {
-                            Some(mode) => Some(mode),
-                            None => self.common_config.report_mode,
-                        },
-                    },
-                )
-            }
-            crate::commands::Commands::Lint(lint_args) => {
-                crate::commands::Commands::Lint(crate::commands::check::LintArgs {
-                    common: crate::commands::check::Common {
-                        file: lint_args.common.file,
-                        except: lint_args
-                            .common
-                            .except
-                            .into_iter()
-                            .chain(self.check_config.except.clone())
-                            .collect(),
-                        deny_warnings: lint_args.common.deny_warnings
-                            || self.check_config.deny_warnings,
-                        deny_notes: lint_args.common.deny_notes || self.check_config.deny_notes,
-                        single_document: lint_args.common.single_document
-                            || self.check_config.single_document,
-                        show_remote_diagnostics: lint_args.common.show_remote_diagnostics
-                            || self.check_config.show_remote_diagnostics,
-                        shellcheck: lint_args.common.shellcheck || self.check_config.shellcheck,
-                        hide_notes: lint_args.common.hide_notes || self.check_config.hide_notes,
-                        no_color: lint_args.common.no_color || !self.common_config.color,
-                        report_mode: match lint_args.common.report_mode {
-                            Some(mode) => Some(mode),
-                            None => self.common_config.report_mode,
-                        },
+                },
+                lint: check_args.lint,
+            }),
+            Commands::ValidateInputs(validate_args) => {
+                Commands::ValidateInputs(validate::ValidateInputsArgs {
+                    document: validate_args.document,
+                    inputs: validate_args.inputs,
+                    no_color: validate_args.no_color || !self.common_config.color,
+                    report_mode: match validate_args.report_mode {
+                        Some(mode) => Some(mode),
+                        None => self.common_config.report_mode,
                     },
                 })
             }
-            crate::commands::Commands::Config(_) => args,
-            crate::commands::Commands::Explain(_) => args,
-            crate::commands::Commands::Analyzer(_) => args,
+            Commands::Lint(lint_args) => Commands::Lint(check::LintArgs {
+                common: check::Common {
+                    file: lint_args.common.file,
+                    except: lint_args
+                        .common
+                        .except
+                        .into_iter()
+                        .chain(self.check_config.except.clone())
+                        .collect(),
+                    deny_warnings: lint_args.common.deny_warnings
+                        || self.check_config.deny_warnings,
+                    deny_notes: lint_args.common.deny_notes || self.check_config.deny_notes,
+                    single_document: lint_args.common.single_document
+                        || self.check_config.single_document,
+                    show_remote_diagnostics: lint_args.common.show_remote_diagnostics
+                        || self.check_config.show_remote_diagnostics,
+                    shellcheck: lint_args.common.shellcheck || self.check_config.shellcheck,
+                    hide_notes: lint_args.common.hide_notes || self.check_config.hide_notes,
+                    no_color: lint_args.common.no_color || !self.common_config.color,
+                    report_mode: match lint_args.common.report_mode {
+                        Some(mode) => Some(mode),
+                        None => self.common_config.report_mode,
+                    },
+                },
+            }),
+            Commands::Config(_) => args,
+            Commands::Explain(_) => args,
+            Commands::Analyzer(_) => args,
         }
     }
 }
