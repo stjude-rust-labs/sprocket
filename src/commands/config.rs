abf2c94692d028114dd12776cd8b186c4d15a9c7
Andrew Thrasher <adthrasher@gmail.com>
2025-05-08 10:51:03 -0400

    feat: --generate to write a sample configuration file

--- src/commands/config.rs
+++ src/commands/config.rs
@@ -2,12 +2,25 @@
 
 use clap::Parser;
 
+use crate::config::Config;
+
 /// Arguments for the `config` subcommand.
 #[derive(Parser, Debug)]
-pub struct ConfigArgs;
+pub struct ConfigArgs {
+    /// Generates a default configuration file.
+    #[arg(long)]
+    generate: bool,
+}
 
 /// Runs the `config` command.
-pub fn config(_args: ConfigArgs, config: crate::config::Config) -> anyhow::Result<()> {
-    println!("{}", toml::to_string_pretty(&config).unwrap_or_default());
+pub fn config(args: ConfigArgs, config: crate::config::Config) -> anyhow::Result<()> {
+    if args.generate {
+        tracing::info!("Generating default configuration file...");
+        let default_config = Config::default();
+        println!("{}", toml::to_string_pretty(&default_config).unwrap_or_default());
+    }
+    else {
+        println!("{}", toml::to_string_pretty(&config).unwrap_or_default());
+    }
     Ok(())
 }
