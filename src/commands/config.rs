e3a58c9ae280a6ded0ab642868e207c47e78152b
Andrew Thrasher <adthrasher@gmail.com>
2025-05-12 10:42:39 -0400

    chore: add subcommands to `config` subcommand

--- src/commands/config.rs
+++ src/commands/config.rs
@@ -1,20 +1,30 @@
 //! Implementation of the config command.
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 use crate::config::Config;
 
 /// Arguments for the `config` subcommand.
-#[derive(Parser, Debug)]
+#[derive(Parser, Debug, Clone)]
 pub struct Args {
-    /// Generates a default configuration file.
-    #[arg(long)]
-    generate: bool,
+    #[command(subcommand)]
+    command: ConfigSubcommand,
 }
 
+/// Subcommands for the `config` command.
+#[derive(Subcommand, Debug, Clone)]
+pub enum ConfigSubcommand {
+    /// Generates a default configuration file.
+    Init,
+
+    /// Displays the current configuration.
+    Resolve,
+}
+
+
 /// Runs the `config` command.
-pub fn config(args: Args, config: crate::config::Config) -> anyhow::Result<()> {
-    if args.generate {
+pub fn config(args: Args, config: Config) -> anyhow::Result<()> {
+    if let ConfigSubcommand::Init = args.command {
         let default_config = Config::default();
         println!(
             "{}",
