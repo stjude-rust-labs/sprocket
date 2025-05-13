91360bbd5536f7960b68b605bc8572d6f4ee018f
Andrew Thrasher <adthrasher@gmail.com>
2025-05-13 10:05:25 -0400

    chore: correct merge artifact

--- src/commands.rs
+++ src/commands.rs
@@ -35,6 +35,9 @@ pub enum Commands {
     #[clap(alias = "fmt")]
     Format(format::Args),
 
+    /// Writes the inputs schema for a WDL document.
+    Inputs(inputs::Args),
+
     /// Lints a document or a directory containing documents.
     Lint(check::LintArgs),
 
