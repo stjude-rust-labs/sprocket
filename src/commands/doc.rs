cd8b445e2c12738cde40b67d6a1df8b38032ffa0
Andrew Frantz <andrew.frantz@stjude.org>
2025-06-02 14:53:42 -0400

    revise: move build steps to wdl

--- src/commands/doc.rs
+++ src/commands/doc.rs
@@ -1,14 +1,15 @@
 //! Implementation of the `doc` command.
 
-use std::path::Path;
 use std::path::PathBuf;
-use std::path::absolute;
 
+use anyhow::bail;
 use anyhow::Ok;
 use anyhow::Result;
-use anyhow::bail;
 use clap::Parser;
 use wdl::doc::document_workspace;
+use wdl::doc::build_stylesheet;
+use wdl::doc::build_web_components;
+use wdl::doc::install_theme;
 
 /// Arguments for the `doc` subcommand.
 #[derive(Parser, Debug)]
@@ -39,48 +40,40 @@ pub struct Args {
     ///
     /// The theme directory is expected to contain a `package.json` file with a
     /// dependency for `"@tailwindcss/cli": "^4.0.0"` and a `src` directory
-    /// with a `main.css` file. `npm install` should be run in the theme
-    /// directory **prior to running this command** to install the dependencies.
+    /// with a `main.css` file. It should also have a `build` script that
+    /// compiles web components into a `dist/index.js` file.
     #[arg(long, value_name = "DIR")]
     pub theme: Option<PathBuf>,
-}
 
-/// Build a stylesheet for the documentation, given a path to the theme
-/// directory.
-pub fn build_stylesheet(theme_dir: &Path) -> Result<PathBuf> {
-    let theme_dir = absolute(theme_dir)?;
-    let output = std::process::Command::new("npx")
-        .arg("@tailwindcss/cli")
-        .arg("-i")
-        .arg("src/main.css")
-        .arg("-o")
-        .arg("dist/style.css")
-        .current_dir(&theme_dir)
-        .output()?;
-    if !output.status.success() {
-        bail!(
-            "failed to build stylesheet: {stderr}",
-            stderr = String::from_utf8_lossy(&output.stderr)
-        );
-    }
-    let css_path = theme_dir.join("dist/style.css");
-    if !css_path.exists() {
-        bail!("failed to build stylesheet: no output file found");
-    }
-
-    Ok(css_path)
+    /// Install the theme if it is not already installed.
+    ///
+    /// Requires the `--theme` argument to be specified.
+    #[arg(long)]
+    pub install: bool,
 }
 
 /// The default output directory for the generated documentation.
 const DEFAULT_OUTPUT_DIR: &str = "docs";
 
 /// Generate documentation for a WDL workspace.
-pub async fn doc(args: Args) -> anyhow::Result<()> {
+pub async fn doc(args: Args) -> Result<()> {
+    if args.install {
+        if let Some(theme_path) = &args.theme {
+            install_theme(theme_path)?;
+        } else {
+            bail!("the --install flag requires the --theme argument to be specified");
+        }
+    }
+
     let css = args
         .theme
         .as_ref()
         .map(|theme| build_stylesheet(theme))
         .transpose()?;
+
+    if let Some(theme) = &args.theme {
+        build_web_components(theme)?;
+    }
 
     let docs_dir = args
         .output
