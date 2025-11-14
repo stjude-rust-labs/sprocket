5ec16dd8e834b7f32d02e51f29e3082252824d53
Adam C. Foltzer <adam.foltzer@stjude.org>
2025-11-14 14:11:43 -0800

    apply review suggestion
    Co-authored-by: Clay McLeod <3411613+claymcleod@users.noreply.github.com>

--- crates/wdl-engine/src/inputs.rs
+++ crates/wdl-engine/src/inputs.rs
@@ -937,8 +937,8 @@ impl Inputs {
         let mut inputs = TaskInputs::default();
         for (key, value) in object {
             // Convert from serde_json::Value to crate::Value
-            let value =
-                Value::try_from(value).with_context(|| format!("invalid input key `{key}`"))?;
+            let value = Value::try_from(value)
+                .with_context(|| format!("invalid input value for key `{key}`"))?;
 
             match key.split_once(".") {
                 Some((prefix, remainder)) if prefix == task.name() => {
