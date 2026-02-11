d09015327fd5cc433c18b15fb98122972201f5ab
Peter Huene <peter@huene.dev>
2026-02-11 13:36:54 -0500

    chore: code review feedback.

--- crates/wdl-engine/src/eval/v1/task.rs
+++ crates/wdl-engine/src/eval/v1/task.rs
@@ -1122,30 +1122,39 @@ impl Evaluator {
             let host = state.path_map.get_by_right(guest).unwrap();
 
             // Check for a host path that is a URL and use the localized path instead
-            let symlink_host_path: Cow<'_, Path> = if self.backend.needs_local_inputs()
-                && is_supported_url(host.as_str())
-                && let Some(local_path) = state.backend_inputs.as_slice().iter().find_map(|i| {
-                    let url = i.path().as_remote()?.as_str();
-                    let host = host.as_str();
+            let symlink_host_path: Cow<'_, Path> =
+                if self.backend.needs_local_inputs() && is_supported_url(host.as_str()) {
+                    state
+                        .backend_inputs
+                        .as_slice()
+                        .iter()
+                        .find_map(|i| {
+                            let url = i.path().as_remote()?.as_str();
+                            let host = host.as_str();
 
-                    // Normalize any trailing slash
-                    if url.strip_suffix('/').unwrap_or(url)
-                        == host.strip_suffix('/').unwrap_or(host)
-                    {
-                        i.local_path()
-                    } else {
-                        None
-                    }
-                }) {
-                Cow::Borrowed(local_path)
-            } else if let Ok(stripped) = symlink_guest_path.strip_prefix(guest.0.as_str())
-                && !stripped.as_os_str().is_empty()
-            {
-                // Translate the guest path to the corresponding host path
-                Cow::Owned(Path::new(host.0.as_str()).join(stripped))
-            } else {
-                Cow::Borrowed(Path::new(host.0.as_str()))
-            };
+                            // Normalize any trailing slash
+                            if url.strip_suffix('/').unwrap_or(url)
+                                == host.strip_suffix('/').unwrap_or(host)
+                            {
+                                Some(Cow::Borrowed(i.local_path()?))
+                            } else {
+                                None
+                            }
+                        })
+                        .with_context(|| {
+                            format!(
+                                "cannot remap symbolic link for guest path `{guest}` because a \
+                                 localized path for URL `{host}` was not found"
+                            )
+                        })?
+                } else if let Ok(stripped) = symlink_guest_path.strip_prefix(guest.0.as_str())
+                    && !stripped.as_os_str().is_empty()
+                {
+                    // Translate the guest path to the corresponding host path
+                    Cow::Owned(Path::new(host.0.as_str()).join(stripped))
+                } else {
+                    Cow::Borrowed(Path::new(host.0.as_str()))
+                };
 
             // Remove the existing link
             remove_file(path).with_context(|| {
