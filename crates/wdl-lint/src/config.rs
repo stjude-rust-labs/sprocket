6c39899742dc8a378081af811bafc562cb557beb
Alex <69764315+Serial-ATA@users.noreply.github.com>
2026-02-27 13:44:27 -0500

    feat(wdl-lint)!: add configuration for `SnakeCase` and `DeclarationName` (#660)
    Co-authored-by: Clay McLeod <3411613+claymcleod@users.noreply.github.com>

--- crates/wdl-lint/src/config.rs
+++ crates/wdl-lint/src/config.rs
@@ -7,6 +7,7 @@ use serde::Serialize;
 
 /// The configuration for lint rules.
 #[derive(Clone, Debug, Default, PartialEq, Serialize, Deserialize)]
+#[serde(default)]
 pub struct Config {
     /// List of keys to ignore in the [`ExpectedRuntimeKeys`] lint.
     ///
@@ -16,6 +17,18 @@ pub struct Config {
     /// allowed_runtime_keys = ["foo"]
     /// ```
     ///
-    /// [`ExpectedRuntimeKeys`]: crate::rules::ExpectedRuntimeKeysRule.
+    /// [`ExpectedRuntimeKeys`]: crate::rules::ExpectedRuntimeKeysRule
     pub allowed_runtime_keys: HashSet<String>,
+    /// List of names to ignore in the [`SnakeCase`] and [`DeclarationName`]
+    /// lints.
+    ///
+    /// ## Example
+    ///
+    /// ```toml
+    /// allowed_names = ["Foo", "counter_int"]
+    /// ```
+    ///
+    /// [`SnakeCase`]: crate::rules::SnakeCaseRule
+    /// [`DeclarationName`]: crate::rules::DeclarationNameRule
+    pub allowed_names: HashSet<String>,
 }
