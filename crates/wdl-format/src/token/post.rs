61bf5a6b3eb141fcc3c064de335cd1358626448e
Ari Frantz <ari.frantz@stjude.org>
2026-03-01 11:39:51 -0500

    Revert "WIP"
    This reverts commit 970827e48224f381cc6fb24d61ed5d56366c664d.

--- crates/wdl-format/src/token/post.rs
+++ crates/wdl-format/src/token/post.rs
@@ -321,8 +321,6 @@ struct TandemBreak {
     pub open: SyntaxKind,
     /// The [`SyntaxKind`] which will close this tandem break.
     pub close: SyntaxKind,
-    // TODO
-    pub alters_indent_level: bool,
     /// Token depth since opening the break.
     ///
     /// The close break is only added when `depth == 0`.
@@ -598,10 +596,8 @@ impl Postprocessor {
                         if top_of_stack.depth > 0 {
                             top_of_stack.depth -= 1;
                         } else {
-                            let top = break_stack.pop().unwrap();
-                            if top.alters_indent_level {
-                                self.indent_level -= 1;
-                            }
+                            break_stack.pop();
+                            self.indent_level -= 1;
                             self.interrupted = true;
                             self.end_line(&mut post_buffer);
                         }
@@ -621,11 +617,10 @@ impl Postprocessor {
                         let tandem_break = TandemBreak {
                             open: *break_kind,
                             close: also_break_on,
-                            alters_indent_level: false,
                             depth: 0,
                         };
                         break_stack.push(tandem_break);
-                        // self.indent_level += 1;
+                        self.indent_level += 1;
                     }
                 }
 
@@ -661,7 +656,6 @@ impl Postprocessor {
                     let tandem_break = TandemBreak {
                         open: *break_kind,
                         close: also_break_on,
-                        alters_indent_level: true,
                         depth: 0,
                     };
                     break_stack.push(tandem_break);
@@ -671,10 +665,8 @@ impl Postprocessor {
         }
 
         // reduce indent for breaks never added
-        for b in break_stack {
-            if b.alters_indent_level {
-                self.indent_level -= 1;
-            }
+        for _ in break_stack {
+            self.indent_level -= 1;
         }
         out_stream.extend(post_buffer);
     }
