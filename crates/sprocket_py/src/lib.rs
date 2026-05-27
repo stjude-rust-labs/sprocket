fa357278fea4aafd651297ebb27ed526f92671c6
BD103 <59022059+BD103@users.noreply.github.com>
2026-05-26 21:25:24 -0400

    docs(py): write docs for python extension

--- crates/sprocket_py/src/lib.rs
+++ crates/sprocket_py/src/lib.rs
@@ -1,7 +1,11 @@
-//! TODO
+//! This crate is a Python extension that exposes a subset `wdl`'s API in Python using [`pyo3`].
+//!
+//! This crate is not meant to be imported directly. Instead, import the `sprocket_py` Python
+//! package (located at `python/sprocket_py`), which bundles this extension.
 
 use pyo3::prelude::*;
 
-/// TODO
+/// Python bindings to [Sprocket](https://sprocket.bio/)'s Workflow Description Language (WDL)
+/// document parsing and linting.
 #[pymodule]
 mod sprocket_py {}
