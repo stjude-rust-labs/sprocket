40592cf0f4784f26d3276d76238b648001a5a011
BD103 <59022059+BD103@users.noreply.github.com>
2026-05-27 21:22:30 -0400

    fix(py): formatting and sorting

--- crates/sprocket_py/src/lib.rs
+++ crates/sprocket_py/src/lib.rs
@@ -1,7 +1,9 @@
-//! This crate is a Python extension that exposes a subset `wdl`'s API in Python using [`pyo3`].
+//! This crate is a Python extension that exposes a subset `wdl`'s API in Python
+//! using [`pyo3`].
 //!
-//! This crate is not meant to be imported directly. Instead, import the `sprocket_py` Python
-//! package (located at `python/sprocket_py`), which bundles this extension.
+//! This crate is not meant to be imported directly. Instead, import the
+//! `sprocket_py` Python package (located at `python/sprocket_py`), which
+//! bundles this extension.
 
 use pyo3::prelude::*;
 
