65839ee082bb63b003a2909ec21706d60aa5b27d
BD103 <59022059+BD103@users.noreply.github.com>
2026-06-13 08:28:33 -0400

    chore(py): make python extension private
    By prefixing the extension module name with an underscore, it simplifies our type stubs down the road and signals that the extension is not meant to be imported directly.

--- python/sprocket_bio/__init__.py
+++ python/sprocket_bio/__init__.py
@@ -1,8 +1,8 @@
 # Import the Python extension (`sprocket_py`).
-from . import sprocket_bio as _sprocket_bio
+from . import _sprocket_bio
 
 # Re-export all items from the Python extension.
-from .sprocket_bio import *
+from ._sprocket_bio import *
 
 __doc__ = _sprocket_bio.__doc__
 # __all__ = _sprocket_bio.__all__
