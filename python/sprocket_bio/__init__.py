7820f6a214114c206434a213cfddd7fb095c0e42
BD103 <59022059+BD103@users.noreply.github.com>
2026-06-24 13:52:58 -0400

    chore(py): add unit testing, type checking, and code formatting (#919)
    Co-authored-by: Clay McLeod <clay.l.mcleod@gmail.com>

--- python/sprocket_bio/__init__.py
+++ python/sprocket_bio/__init__.py
@@ -1,5 +1,8 @@
-# Re-export all items from the Python extension.
-from .sprocket_bio import *
+# Import the Python extension (`_sprocket_bio.so` on Unix and `_sprocket_bio.pyd` on Windows).
+from . import _sprocket_bio
 
-__doc__ = sprocket_bio.__doc__
-# __all__ = sprocket_bio.__all__
+# Re-export all items from the Python extension.
+from ._sprocket_bio import *
+
+__doc__ = _sprocket_bio.__doc__
+# __all__ = _sprocket_bio.__all__
