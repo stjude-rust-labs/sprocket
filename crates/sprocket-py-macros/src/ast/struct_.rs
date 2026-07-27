62e1c210103b767d9db1cf06bd1c3f5ab8780708
BD103 <59022059+BD103@users.noreply.github.com>
2026-07-27 15:20:32 -0400

    refactor(py-macros): rewrite `#[ast]` struct builder

--- crates/sprocket-py-macros/src/ast/struct_.rs
+++ crates/sprocket-py-macros/src/ast/struct_.rs
@@ -1,111 +1,72 @@
 //! TODO
 
-use proc_macro2::Ident;
 use proc_macro2::TokenStream;
 use quote::format_ident;
 use quote::quote;
-use syn::Attribute;
 use syn::Fields;
 use syn::Generics;
 use syn::ItemStruct;
 use syn::LitStr;
-use syn::Path;
 use syn::parse_quote;
 
 /// Gives the Python binding equivalent of an AST struct.
 pub(crate) fn build(original: &ItemStruct) -> syn::Result<TokenStream> {
-    verify_generics(&original.generics)?;
-    verify_fields(&original.fields)?;
+    let mut py_struct = original.clone();
 
-    let py_struct = py_struct(&original);
-    let py_impl = py_impl(&original, &py_struct.ident);
+    py_struct.ident = format_ident!("Py{}", original.ident);
 
-    Ok(quote! {
-        #py_struct
-        #py_impl
-    })
-}
+    // Only copy over doc comments, remove all other attributes.
+    py_struct.attrs.retain(|attr| attr.path().is_ident("doc"));
 
-fn verify_generics(_generics: &Generics) -> syn::Result<()> {
-    // TODO: Assert generics are just `<N: TreeNode = SyntaxNode>`.
-    Ok(())
-}
-
-fn verify_fields(fields: &Fields) -> syn::Result<()> {
-    let Fields::Unnamed(fields) = fields else {
-        return Err(syn::Error::new_spanned(
-            fields,
-            "`#[ast]` only supports tuple structs",
-        ));
-    };
-
-    if fields.unnamed.len() != 1 {
-        return Err(syn::Error::new_spanned(
-            fields,
-            "`#[ast]` only supports structs with a single field",
-        ));
-    }
-
-    Ok(())
-}
-
-fn py_attrs(original_attrs: &[Attribute], class_name: &Ident) -> Vec<Attribute> {
-    let doc_path: Path = parse_quote!(doc);
-
-    // Extract all `#[doc = "..."]` attributes from the original struct.
-    let doc_attrs = original_attrs
-        .iter()
-        .filter(move |attr| *attr.path() == doc_path)
-        .cloned();
-
-    let class_name = LitStr::new(&class_name.to_string(), class_name.span());
-
-    let additional_attrs = [
+    py_struct.attrs.extend_from_slice(&[
         // Make generated struct a pyclass.
-        parse_quote!(#[::pyo3::pyclass(module = "sprocket_bio.ast.v1", name = #class_name, extends = crate::PyAstNode, frozen, skip_from_py_object, eq)]),
+        {
+            let class_name = LitStr::new(&original.ident.to_string(), original.ident.span());
+            parse_quote!(#[::pyo3::pyclass(module = "sprocket_bio.ast.v1", name = #class_name, extends = crate::PyAstNode, frozen, skip_from_py_object, eq)])
+        },
         // `#[pymethods]` relies on the Python struct being cloneable. We additionally derive
         // `PartialEq` for parity with the original struct.
         parse_quote!(#[derive(Clone, PartialEq)]),
         // `Debug` is purposefully not implemented, silence the lint.
         parse_quote!(#[allow(missing_debug_implementations)]),
-    ];
+    ]);
 
-    doc_attrs.chain(additional_attrs).collect()
-}
-
-fn py_struct(original: &ItemStruct) -> ItemStruct {
-    let ItemStruct {
-        attrs,
-        vis,
-        struct_token,
-        ident,
-        semi_token,
-        ..
-    } = original;
-
-    // Only copy over doc comments, not other attributes.
-    let py_attrs = py_attrs(attrs, ident);
-
-    // Prepend "Py" to the beginning of the name.
-    let py_ident = format_ident!("Py{ident}");
-
-    ItemStruct {
-        attrs: py_attrs,
-        vis: vis.clone(),
-        struct_token: *struct_token,
-        ident: py_ident.clone(),
-        // Generics are removed.
-        generics: Generics::default(),
-        // Replace generic `SyntaxNode` field with thread-safe variant.
-        fields: Fields::Unnamed(parse_quote!((crate::python::ThreadSafeSyntaxNode))),
-        semi_token: *semi_token,
+    // Verify generics.
+    if original.generics != parse_quote!(<N: TreeNode = SyntaxNode>) {
+        return Err(syn::Error::new_spanned(
+            &original.generics,
+            "`#[ast]` requires that struct generics be `<N: TreeNode = SyntaxNode>`",
+        ));
     }
-}
 
-fn py_impl(original: &ItemStruct, py_ident: &Ident) -> TokenStream {
-    let ItemStruct { ident, .. } = original;
+    // Remove generics.
+    py_struct.generics = Generics::default();
 
-    quote! {
+    // Verify fields.
+    if let Fields::Unnamed(ref fields) = original.fields {
+        if fields.unnamed.len() != 1 {
+            return Err(syn::Error::new_spanned(
+                fields,
+                "`#[ast]` only supports structs with a single field",
+            ));
+        }
+    } else {
+        return Err(syn::Error::new_spanned(
+            &original.fields,
+            "`#[ast]` only supports tuple structs",
+        ));
+    }
+
+    // Replace generic `N: SyntaxNode` field with thread-safe variant.
+    py_struct.fields = Fields::Unnamed(parse_quote!((crate::python::ThreadSafeSyntaxNode)));
+
+    // Used for quote formatting.
+    let ident = &original.ident;
+    let py_ident = &py_struct.ident;
+
+    Ok(quote! {
+        #py_struct
+
         // Converting from the original struct to the Python struct. This is used in the original
         // struct's `IntoPyObject` impl.
         impl ::std::convert::From<#ident> for #py_ident {
@@ -138,5 +99,5 @@ fn py_impl(original: &ItemStruct, py_ident: &Ident) -> TokenStream {
                     .map(Bound::into_any)
             }
         }
-    }
+    })
 }
