164c7b79540bbf1504d6d0d2251a1f57bd77329b
BD103 <59022059+BD103@users.noreply.github.com>
2026-07-27 21:57:32 -0400

    refactor(py-macros): extract `#[ast]` ast kind logic

--- crates/sprocket-py-macros/src/ast.rs
+++ crates/sprocket-py-macros/src/ast.rs
@@ -49,7 +49,7 @@ impl Default for Args {
 }
 
 /// Represents whether an AST element is a node or token.
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq, Debug)]
 enum AstKind {
     Node,
     Token,
@@ -61,18 +61,7 @@ pub(crate) fn build(original: &ItemStruct, args: Args) -> Result<TokenStream> {
 
     build_ident(&mut py_struct, original);
 
-    // Determine if struct is a node or a token.
-    let ast_kind = if original.generics == parse_quote!(<N: TreeNode = SyntaxNode>) {
-        AstKind::Node
-    } else if original.generics == parse_quote!(<T: TreeToken = SyntaxToken>) {
-        AstKind::Token
-    } else {
-        return Err(Error::new_spanned(
-            &original.generics,
-            "`#[ast]` requires that struct generics be either `<N: TreeNode = SyntaxNode>` or \
-             `<T: TreeToken = SyntaxToken>`",
-        ));
-    };
+    let ast_kind = ast_kind(original)?;
 
     // Remove generics.
     py_struct.generics = Generics::default();
@@ -177,6 +166,22 @@ fn build_ident(py_struct: &mut ItemStruct, original: &ItemStruct) {
     py_struct.ident = format_ident!("Py{}", original.ident);
 }
 
+/// Determines if the AST element is a node or token by inspecting its generic
+/// types.
+fn ast_kind(original: &ItemStruct) -> Result<AstKind> {
+    if original.generics == parse_quote!(<N: TreeNode = SyntaxNode>) {
+        Ok(AstKind::Node)
+    } else if original.generics == parse_quote!(<T: TreeToken = SyntaxToken>) {
+        Ok(AstKind::Token)
+    } else {
+        return Err(Error::new_spanned(
+            &original.generics,
+            "`#[ast]` requires that struct generics be either `<N: TreeNode = SyntaxNode>` or \
+             `<T: TreeToken = SyntaxToken>`",
+        ));
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -216,5 +221,53 @@ mod tests {
         build_ident(&mut py_struct, &original);
 
         assert_eq!(py_struct.ident.to_string(), "PyFoo");
+    }
+
+    #[test]
+    fn ast_kind_node() {
+        let original: ItemStruct = parse_quote! { struct Foo<N: TreeNode = SyntaxNode>(N); };
+        assert_eq!(ast_kind(&original).unwrap(), AstKind::Node);
+    }
+
+    #[test]
+    fn ast_kind_token() {
+        let original: ItemStruct = parse_quote! { struct Foo<T: TreeToken = SyntaxToken>(T); };
+        assert_eq!(ast_kind(&original).unwrap(), AstKind::Token);
+    }
+
+    #[test]
+    fn ast_kind_invalid_generics() {
+        let missing_generics: ItemStruct = parse_quote! { struct Foo; };
+        let result = ast_kind(&missing_generics);
+
+        assert!(
+            result.is_err(),
+            "did not error on missing generics: {result:?}"
+        );
+
+        let different_name: ItemStruct =
+            parse_quote! { struct Foo<BAD: TreeToken = SyntaxToken>(BAD); };
+        let result = ast_kind(&different_name);
+
+        assert!(
+            result.is_err(),
+            "did not error on incorrect generic name: {result:?}"
+        );
+
+        let different_trait: ItemStruct = parse_quote! { struct Foo<N: Display = String>(N); };
+        let result = ast_kind(&different_trait);
+
+        assert!(
+            result.is_err(),
+            "did not error on incorrect generic trait bound: {result:?}"
+        );
+
+        let missing_default: ItemStruct = parse_quote! { struct Foo<N: TreeNode>(N); };
+        let result = ast_kind(&missing_default);
+
+        assert!(
+            result.is_err(),
+            "did not error on missing generic default: {result:?}"
+        );
     }
 }
