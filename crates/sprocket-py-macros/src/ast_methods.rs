a144c2ff2ed4c99005d121953cfce08aa91d1863
BD103 <59022059+BD103@users.noreply.github.com>
2026-07-30 09:04:39 -0400

    feat(py-macros): annotate generated `impl` with `#[pymethods]`

--- crates/sprocket-py-macros/src/ast_methods.rs
+++ crates/sprocket-py-macros/src/ast_methods.rs
@@ -5,17 +5,22 @@ use quote::quote;
 use syn::ItemImpl;
 use syn::Result;
 use syn::parse::Nothing;
+use syn::parse_quote;
 
 pub(crate) fn ast_methods(
     args_stream: TokenStream,
     impl_stream: TokenStream,
 ) -> Result<TokenStream> {
     syn::parse2::<Nothing>(args_stream)?;
-    let mut impl_ = syn::parse2::<ItemImpl>(impl_stream)?;
+    let impl_ = syn::parse2::<ItemImpl>(impl_stream)?;
 
-    todo!();
+    let mut py_impl = impl_.clone();
+
+    // Annotate the Python `impl` with `#[pymethods]`.
+    py_impl.attrs.push(parse_quote!(#[::pyo3::pymethods]));
 
     Ok(quote! {
         #impl_
+        #py_impl
     })
 }
