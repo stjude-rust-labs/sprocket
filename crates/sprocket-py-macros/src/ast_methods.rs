7306d8bb365e63c4f2f21ad9a34aaa4b2156b001
BD103 <59022059+BD103@users.noreply.github.com>
2026-07-30 10:32:28 -0400

    feat(py-macros): process self type in `#[ast_methods]`

--- crates/sprocket-py-macros/src/ast_methods.rs
+++ crates/sprocket-py-macros/src/ast_methods.rs
@@ -1,14 +1,20 @@
 //! TODO
 
 use proc_macro2::TokenStream;
+use quote::format_ident;
 use quote::quote;
 use syn::Error;
 use syn::Generics;
 use syn::Ident;
 use syn::ItemImpl;
+use syn::PathArguments;
 use syn::Result;
+use syn::Type;
+use syn::TypeGroup;
+use syn::TypeParen;
 use syn::parse::Nothing;
 use syn::parse_quote;
+use syn::spanned::Spanned;
 
 /// Represents whether an AST element is a node or token.
 #[derive(PartialEq, Debug)]
@@ -37,6 +43,9 @@ pub(crate) fn ast_methods(
 
     // Remove the first generic (`impl<N: TreeNode> Ast<N>` into `impl Ast<N>`).
     py_impl.generics = Generics::default();
+
+    // Remove second generic and add "Py" prefix (`impl Ast<N>` into `impl PyAst`).
+    make_py_self_ty(&mut py_impl.self_ty)?;
 
     Ok(quote! {
         #original
@@ -84,9 +93,53 @@ fn ast_kind(generics: &Generics) -> Result<Option<AstKind>> {
     }
 }
 
+/// Adds the "Py" prefix to the `self_ty`'s ident and removes its generic
+/// parameters.
+fn make_py_self_ty(self_ty: &mut Type) -> Result<()> {
+    let type_path = match self_ty {
+        // When encountering a group or parenthesized type, recurse into the inner element.
+        Type::Group(TypeGroup { elem, .. }) | Type::Paren(TypeParen { elem, .. }) => {
+            *self_ty = *elem.clone();
+            return make_py_self_ty(self_ty);
+        }
+        Type::Path(type_path) => type_path,
+        _ => {
+            return Err(Error::new_spanned(
+                &self_ty,
+                "type not supported by `#[ast_methods]`",
+            ));
+        }
+    };
+
+    if let Some(ref qself) = type_path.qself {
+        return Err(Error::new(
+            qself.span(),
+            "qualified paths are not supported by `#[ast_methods]`",
+        ));
+    }
+
+    let last_segment = type_path
+        .path
+        .segments
+        .last_mut()
+        .expect("type paths should contain at least one segment");
+
+    last_segment.ident = format_ident!("Py{}", last_segment.ident);
+
+    // TODO: Verify only argument is `AstKind` ident.
+    last_segment.arguments = PathArguments::None;
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use proc_macro2::Span;
+    use syn::Path;
+    use syn::TypeGroup;
+    use syn::TypePath;
+    use syn::punctuated::Punctuated;
+    use syn::token::Group;
 
     use super::*;
 
@@ -161,5 +214,84 @@ mod tests {
             result.is_err(),
             "did not error on invalid trait bound: {result:?}"
         );
+    }
+
+    #[test]
+    fn self_ty() {
+        let mut self_ty: Type = parse_quote!(Ast<T>);
+
+        make_py_self_ty(&mut self_ty).unwrap();
+
+        let expected: Type = parse_quote!(PyAst);
+
+        assert_eq!(self_ty, expected);
+    }
+
+    #[test]
+    fn self_ty_group() {
+        let mut self_ty: Type = parse_quote!(Ast<T>);
+        self_ty = Type::Group(TypeGroup {
+            attrs: Vec::new(),
+            group_token: Group::default(),
+            elem: Box::new(self_ty),
+        });
+
+        make_py_self_ty(&mut self_ty).unwrap();
+
+        let expected: Type = parse_quote!(PyAst);
+
+        assert_eq!(self_ty, expected);
+    }
+
+    #[test]
+    fn self_ty_paren() {
+        let mut self_ty: Type = parse_quote!((Ast<T>));
+
+        make_py_self_ty(&mut self_ty).unwrap();
+
+        let expected: Type = parse_quote!(PyAst);
+
+        assert_eq!(self_ty, expected);
+    }
+
+    #[test]
+    fn self_ty_unsupported_type() {
+        let mut self_ty: Type = parse_quote!([Ast<T>; 2]);
+
+        let result = make_py_self_ty(&mut self_ty);
+
+        assert!(
+            result.is_err(),
+            "did not error on unsupported `self_ty`: {self_ty:?}"
+        );
+    }
+
+    #[test]
+    fn self_ty_qself() {
+        let mut self_ty: Type = parse_quote!(<SyntaxNode as TreeNode>::Token);
+
+        let result = make_py_self_ty(&mut self_ty);
+
+        assert!(
+            result.is_err(),
+            "did not error on qualified path: {self_ty:?}"
+        );
+    }
+
+    #[test]
+    #[should_panic = "type paths should contain at least one segment"]
+    fn self_ty_empty_path() {
+        // Manually construct the empty path, as it's not possible with
+        // `parse_quote!()`.
+        let mut self_ty = Type::Path(TypePath {
+            attrs: Vec::new(),
+            qself: None,
+            path: Path {
+                leading_colon: None,
+                segments: Punctuated::new(),
+            },
+        });
+
+        let _ = make_py_self_ty(&mut self_ty);
     }
 }
