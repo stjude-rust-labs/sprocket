c01a479b2e6203ed2f9dcbd7980a40107db9f62f
Peter Huene <peter@huene.dev>
2026-08-05 12:46:36 -0400

    Update crates/wdl-engine/src/backend/lsf_apptainer.rs
    Co-authored-by: Clay McLeod <3411613+claymcleod@users.noreply.github.com>

--- crates/wdl-engine/src/backend/lsf_apptainer.rs
+++ crates/wdl-engine/src/backend/lsf_apptainer.rs
@@ -120,7 +120,7 @@ fn truncate_job_name(name: &str) -> &str {
 /// Writes a `bsub_command` file with the given `bsub` command.
 async fn write_bsub_command_file(dir: &Path, command: &Command) -> Result<()> {
     let path = dir.join(BSUB_COMMAND_FILE_NAME);
-    fs::write(&path, format!("{command:?}\n"))
+    fs::write(&path, format!("{command:?}\n", command = command.as_std()))
         .await
         .with_context(|| format!("failed to write file `{path}`", path = path.display()))?;
     Ok(())
