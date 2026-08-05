d3ae7b9a837332f21dfbda84da12927fa880ca3d
BD103 <59022059+BD103@users.noreply.github.com>
2026-08-05 17:33:10 -0400

    feat(py-macros): swap inner variant type with python equivalent for `#[ast]` enums

--- crates/sprocket-py-macros/src/ast/enum_.rs
+++ crates/sprocket-py-macros/src/ast/enum_.rs
@@ -1,15 +1,21 @@
 //! TODO
 
 use proc_macro2::TokenStream;
-use quote::quote;
+use quote::ToTokens;
+use quote::format_ident;
+use syn::Error;
+use syn::Fields;
 use syn::Generics;
 use syn::ItemEnum;
 use syn::LitStr;
+use syn::PathArguments;
 use syn::Result;
+use syn::Type;
 use syn::parse_quote;
 
 use super::Args;
 
+/// Builds the Python binding equivalent of an AST enum.
 pub(super) fn build(original: &ItemEnum, args: Args) -> Result<TokenStream> {
     let mut py_enum = original.clone();
 
@@ -20,11 +26,43 @@ pub(super) fn build(original: &ItemEnum, args: Args) -> Result<TokenStream> {
 
     make_py_attrs(&mut py_enum, original, args);
 
-    todo!();
+    // Strip generic from all variants, add "Py" prefix
+    for variant in py_enum.variants.iter_mut() {
+        let fields = match variant.fields {
+            Fields::Unnamed(ref mut fields) => fields,
+            Fields::Unit => continue,
+            _ => {
+                return Err(Error::new_spanned(
+                    &variant.fields,
+                    "`#[ast]` does not support struct variants",
+                ));
+            }
+        };
 
-    Ok(quote! {
-        #py_enum
-    })
+        if fields.unnamed.is_empty() {
+            continue;
+        }
+
+        if fields.unnamed.len() > 1 {
+            return Err(Error::new_spanned(
+                fields,
+                "`#[ast]` requires enums variants have zero or one fields",
+            ));
+        }
+
+        if let Type::Path(ref mut type_path) = fields.unnamed.first_mut().unwrap().ty {
+            let segment = type_path
+                .path
+                .segments
+                .last_mut()
+                .expect("paths are expected to have at least one segment");
+
+            segment.ident = format_ident!("Py{}", segment.ident);
+            segment.arguments = PathArguments::None
+        }
+    }
+
+    Ok(py_enum.to_token_stream())
 }
 
 /// Modifies the attributes of `py_enum`.
@@ -44,8 +82,9 @@ fn make_py_attrs(py_enum: &mut ItemEnum, original: &ItemEnum, args: Args) {
 
             parse_quote!(#[::pyo3::pyclass(module = #module, name = #class_name, frozen, from_py_object, eq)])
         },
-        // We derive `PartialEq` for parity with the original enum.
-        parse_quote!(#[derive(PartialEq)]),
+        // `Clone` is required by `from_py_object`, and `PartialEq` is for parity with the original
+        // enum.
+        parse_quote!(#[derive(Clone, PartialEq)]),
         // `Debug` is purposefully not implemented, silence the lint.
         parse_quote!(#[allow(missing_debug_implementations)]),
     ]);
