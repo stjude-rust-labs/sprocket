43207b7db8f10667c010b4fed852606192534473
BD103 <59022059+BD103@users.noreply.github.com>
2026-08-17 15:40:14 -0400

    docs(py): configure sphinx autodoc

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -9,6 +9,8 @@ copyright = "%Y, Sprocket Contributors"
 # General configuration
 
 extensions = [
+    # Automatically generate API docs.
+    "sphinx.ext.autodoc",
     # Track how long it takes to render pages.
     "sphinx.ext.duration",
     # Sphinx Read the Docs theme.
@@ -20,3 +22,13 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # Options for HTML output
 
 html_theme = "sphinx_rtd_theme"
+
+# Extension configuration
+
+autodoc_member_order = "groupwise"
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+}
+autodoc_use_type_comments = False
